d15950f815d68e07199f53c7d4096f9f35f8f307
Valtýr Örn Kjartansson <valtyr@gmail.com>
2018-03-11 01:59:59 +0100

    Remove Nanoid from index

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-var nanoid = require('nanoid')
 var adjectives = require('./adjectives')
 var nouns = require('./nouns')
 
 // adjectives and nouns from https://gist.github.com/afriggeri/1266756
 
 module.exports = function() {
-  var id = nanoid(7)
   var adjectiveIndex = Math.floor(Math.random() * adjectives.length)
   var nounIndex = Math.floor(Math.random() * nouns.length)
   return adjectives[adjectiveIndex] + "-" + nouns[nounIndex] + "-" + id
